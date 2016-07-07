25931539d87c1f250f97cbed1623f9c0d3b7def2
na0na0 <gflorian@student.42.fr>
2016-07-07 20:11:45 +0200

    verbose cp and remove gpg

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cp .aliases .curlrc .exports .functions .gdbinit .gemrc .gitconfig .lldbinit \
+cp -v .aliases .curlrc .exports .functions .gdbinit .gemrc .gitconfig .lldbinit \
    .psqlrc .tmux.conf .vimrc .wgetrc .zshrc krb5.conf $HOME
